af7b22f0a2796cc024d7f95e47107c90b243a9b5
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-12-03 21:55:45 +0100

    adding Reciepe cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/Pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>hello</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Switch>
+      <Route path='/' exact component={Home}/>
+      </Switch>
+    </Router>
+   
+    </>
   );
 }
 
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css';
+import React from 'react';
+import Header from '../Header';
+import Cards from '../Cards';
+
+ function Home  () {
+    return (
+        <>
+         <Header />   
+         <Cards />
+        </>
+    );
+}
+export default Home;
