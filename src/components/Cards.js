8c1df887a2303a17c102454f8f6545ee21ad1ce1
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-12-08 23:31:20 +0100

    Home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,19 +9,19 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem src='images/img-9.jpg'
-                        text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                        <CardItem src='images/img-1.jpg'
+                        text='CRANBERRY WHITECHOCOLATE FUDGE'
                         label='To The Reciepe'
                         path='/services'
                         />
                         <CardItem src='images/img-2.jpg'
-                      text='Travel through the Islands of Bali in a Private Cruise'
+                      text=' NO BAKE CHOCOLATE FUDGE – 3 INGREDIENTS ONLY!'
                       label='To the Reciepe'
                        path='/services'
                         />
                         <CardItem
-                      src='images/img-2.jpg'
-                      text='Travel through the Islands of Bali in a Private Cruise'
+                      src='images/img-3.jpeg'
+                      text='VANILLA SPONGE CAKE – FLUFFY TEA CAKE RECIPE!'
                       label='To the Reciepe'
                       path='/services'
                         />
@@ -29,14 +29,14 @@ function Cards() {
                         </ul>
                           <ul className="cards__items">
                           <CardItem
-                    src='images/img-2.jpg'
-                    text='Travel through the Islands of Bali in a Private Cruise'
+                    src='images/img-4.jpeg'
+                    text='LEBANEESE OLIVE PIZZA RECIPE'
                     label='To the Reciepe'
                     path='/services'
                         />
                         <CardItem
-                    src='images/img-2.jpg'
-                    text='Travel through the Islands of Bali in a Private Cruise'
+                    src='images/img-5.jpg'
+                    text='EGGLESS VANILLA SPONGE CAKE RECIPE'
                     label='To the Reciepe'
                     path='/services'
                         />
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,12 +2,13 @@ import '../../App.css';
 import React from 'react';
 import Header from '../Header';
 import Cards from '../Cards';
-
+import Footer from '../Footer';
  function Home  () {
     return (
         <>
          <Header />   
          <Cards />
+         <Footer />
         </>
     );
 }
